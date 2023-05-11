e31cb7ccfe64cf2c53915e7f6dad54890e9b5ec3
asura7969 <1402357969@qq.com>
2023-05-11 22:19:13 +0800

    [feat]-iter

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 * Rust内存数据布局
   * [布局-1](https://rustmagazine.github.io/rust_magazine_2021/chapter_6/ant-rust-data-layout.html)
   * [布局-2](https://github.com/rustlang-cn/Rustt/blob/main/Articles/%5B2022-05-04%5D%20%E5%8F%AF%E8%A7%86%E5%8C%96%20Rust%20%E5%90%84%E6%95%B0%E6%8D%AE%E7%B1%BB%E5%9E%8B%E7%9A%84%E5%86%85%E5%AD%98%E5%B8%83%E5%B1%80.md)
-* `static` 关键字
+* `lifetime`
+  * [生命周期](https://xie.infoq.cn/article/caae061fc5578083b1c45e470)
 * Rust std容器使用
 * 原子类
 * 并发
@@ -15,3 +16,4 @@
 * bytes
 * [async-rdma](https://rustmagazine.github.io/rust_magazine_2022/Q1/contribute/async-rdma.html)
 * [tracing](https://github.com/rustlang-cn/Rustt/blob/main/Articles/%5B2022-04-07%5D%20%E5%9C%A8%20Rust%20%E4%B8%AD%E4%BD%BF%E7%94%A8%20tracing%20%E8%87%AA%E5%AE%9A%E4%B9%89%E6%97%A5%E5%BF%97.md)
+* lazy_static
---
+++ base/src/iter.rs
@@ -0,0 +1,44 @@
+
+#[derive(Debug)]
+pub struct MyIter(Vec<usize>);
+
+impl Iterator for MyIter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct MyData(usize, usize);
+
+impl Default for MyData {
+    fn default() -> Self {
+        MyData(0,1)
+    }
+}
+
+impl IntoIterator for MyData {
+    type Item = usize;
+    type IntoIter = MyIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        MyIter(vec![self.0, self.1])
+    }
+}
+
+#[cfg(test)]
+mod iter_tests {
+    use crate::iter::{MyData, MyIter};
+
+    #[test]
+    #[ignore]
+    pub fn test_into_iter() {
+        let mut iter = MyData::default().into_iter();
+        while let Some(value) = iter.next() {
+            println!("value: {value:?}")
+        }
+        println!("{iter:?}")
+    }
+
+}
