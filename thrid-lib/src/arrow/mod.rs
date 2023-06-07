f6dd0548b760b4044a9620d343f85e1acd4b30fc
Asura7969 <1402357969@qq.com>
2023-06-07 16:46:19 +0800

    async read parquet file

--- thrid-lib/src/arrow/mod.rs
+++ thrid-lib/src/arrow/mod.rs
@@ -1,13 +1,54 @@
-#[warn(dead_code)]
-#[warn(unused_imports)]
 #[cfg(test)]
 mod tests {
+    use anyhow::{Ok, Result};
     use arrow::array::*;
+    use arrow_cast::pretty::print_batches;
     use arrow_csv::reader::Format;
     use arrow_csv::ReaderBuilder;
     use arrow_schema::*;
+    use futures::TryStreamExt;
+    use parquet::arrow::arrow_reader::{ArrowPredicateFn, RowFilter};
+    use parquet::arrow::{ParquetRecordBatchStreamBuilder, ProjectionMask};
     use std::io::Seek;
+    use std::time::SystemTime;
     use std::{fs::File, sync::Arc};
+
+    #[tokio::test]
+    async fn test_async_read_parquet_file() -> Result<()> {
+        let file = tokio::fs::File::open("src/test/data/alltypes_plain.parquet").await?;
+
+        let mut builder = ParquetRecordBatchStreamBuilder::new(file)
+            .await
+            .unwrap()
+            .with_batch_size(8192);
+
+        let file_metadata = builder.metadata().file_metadata().clone();
+        let mask = ProjectionMask::roots(file_metadata.schema_descr(), [0, 1, 2]);
+
+        // 设置只读取 0, 1, 2 列
+        builder = builder.with_projection(mask);
+
+        let filter = ArrowPredicateFn::new(
+            ProjectionMask::roots(file_metadata.schema_descr(), [0]),
+            // 定义 where 条件, 0列(id)值 > 1
+            |record_batch| arrow::compute::gt_dyn_scalar(record_batch.column(0), 1),
+        );
+        let row_filter = RowFilter::new(vec![Box::new(filter)]);
+        builder = builder.with_row_filter(row_filter);
+
+        // Build a async parquet reader.
+        let stream = builder.build().unwrap();
+
+        let start = SystemTime::now();
+
+        let result = stream.try_collect::<Vec<_>>().await?;
+
+        println!("took: {} ms", start.elapsed().unwrap().as_millis());
+
+        print_batches(&result).unwrap();
+
+        Ok(())
+    }
 
     #[test]
     fn test_read_csv_file() {
